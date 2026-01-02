ed932379fc266b6074b9cc3ab1bf921518118d33
FlyingMatrix <jxzhang.acm@gmail.com>
2026-01-02 14:31:13 +0100

    leetcode 2 - add two numbers

---
+++ 2 - add two numbers.rs
@@ -0,0 +1,60 @@
+/* 2 - Add Two Numbers
+
+    You are given two non-empty linked lists representing two non-negative integers.
+    The digits are stored in reverse order, and each of their nodes contains a single digit.
+    Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+
+    Example 2:
+        Input: l1 = [0], l2 = [0]
+        Output: [0]
+
+    Example 3:
+        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        Output: [8,9,9,9,0,0,0,1]
+ */
+
+/* My accepted solution */
+//
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut carry = 0;
+        let mut dummy = Box::new(ListNode::new(0));
+        let mut current = &mut dummy;
+
+        while l1.is_some() || l2.is_some() || carry != 0{
+            let mut val_l1 = l1.as_ref().map(|node| node.val).unwrap_or(0);
+            let mut val_l2 = l2.as_ref().map(|node| node.val).unwrap_or(0);
+            let mut total = val_l1 + val_l2 + carry;
+
+            current.next = Some(Box::new(ListNode::new(total % 10)));
+            current = current.next.as_mut().unwrap();
+            carry = total / 10;
+
+            l1 = l1.and_then(|node| node.next);
+            l2 = l2.and_then(|node| node.next);
+        }
+        dummy.next
+    }
+}
